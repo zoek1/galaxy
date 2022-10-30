21653f020f47dea062918f4f80201e8069beca30
Monica <moonniy.2012@gmail.com>
2022-10-30 01:15:10 -0600

    Question page added

--- dapp/src/pages/ViewCampaignPage.js
+++ dapp/src/pages/ViewCampaignPage.js
@@ -1,27 +1,32 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
 import IntegrationButton from '../components/IntegrationButton'
+import { Link } from "react-router-dom";
 
 const Fields = [
   {
     id: 1,
     name: 'Discord',
-    image: 'bi bi-discord'
+    image: 'bi bi-discord',
+    integration_action: 'link_account',
   },
   {
     id: 2,
     name: 'Join Telegram',
-    image: 'bi bi-telegram'
+    image: 'bi bi-telegram',
+    integration_action: 'link_account',
   },
   {
     id: 3,
     name: 'Suscribe Newsletter',
-    image: 'bi bi-newspaper'
+    image: 'bi bi-newspaper',
+    integration_action: 'link_account',
   },
   {
     id: 4,
     name: 'What do you thing?',
-    image: 'bi bi-question'
+    image: 'bi bi-question',
+    integration_action: 'ask_question',
   }
 ]
 
@@ -33,7 +38,13 @@ function ViewCampaignPage (){
         {
           Fields.map(field =>(
             <div key={field.id}>
-              <IntegrationButton name={field.name} imageIcon={field.image}/>
+            { field.integration_action === 'ask_question' ?
+              <Link to={'/think'}>
+                <IntegrationButton id={field.id} name={field.name} imageIcon={field.image}/>
+              </Link>
+              : <IntegrationButton id={field.id} name={field.name} imageIcon={field.image}/>
+            } 
+
             </div>
           ))
         }
