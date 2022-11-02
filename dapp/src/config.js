a061197022736e2c9a82ae5c594cd1c798b701c3
Miguel Gordian <os.aioria@gmail.com>
2022-11-02 14:47:10 -0600

    Add dapp config file

---
+++ dapp/src/config.js
@@ -0,0 +1,9 @@
+const LOYALTY_CONTRACT = "KT1WWwuMW19d7d1cTwMthiWHiEbDJK1DVvZg"
+const LOYALTY_ADMIN = "tz1cSjTrRBE2UZGJ4xduR3Kw71vKur8J4Uxb"
+const GHOSTNET_RPC = 'https://ghostnet.ecadinfra.com';
+
+export default {
+    LOYALTY_CONTRACT,
+    LOYALTY_ADMIN,
+    GHOSTNET_RPC
+}
